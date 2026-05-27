e0124de5387ec07b0d9f73c07c995f95f579583d
Danil Chernov <realdnchka@proton.me>
2026-05-27 17:34:13 +0700

    Developed classificator for spam filtering, created a model in models/

--- main.py
+++
@@ -1,9 +0,0 @@
-def main():
-    import torch
-    print(torch.__version__)
-    print(torch.backends.mps.is_available())
-    print(torch.backends.mps.is_built())
-
-
-if __name__ == "__main__":
-    main()
